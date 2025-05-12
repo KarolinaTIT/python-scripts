a50c31a13ddc5fdbbd1c3db1122c50d60dc68265
KarolinaTIT <karolinaseblikina07@gmail.com>
2025-05-12 14:59:13 +0300

    Update 06_execution_time.py

--- scripts/06_execution_time.py
+++ scripts/06_execution_time.py
@@ -12,9 +12,6 @@ For example:
 """
 
 
-import time
-import random
-
 
 class ExecutionTime:
     def __init__(self):
@@ -22,13 +19,21 @@ class ExecutionTime:
 
     def duration(self):
         return time.time() - self.start_time
+ 
+ timer = ExecutionTime()
+ 
+ for i in range(1,1000000):
+     if i % 2 == 0:
+         sample_list.apppend(random.randint(1, 888898))
+  
+  for i in range(10)
+      print("11111")
+         
+         
+ print('Finished in {} seconds.'.format(timer.duration()))        
+# sample_list = list()
+# my_list = [random.randint(1, 888898) for num in
+#            range(1, 1000000) if num % 2 == 0]
+# print('Finished in {} seconds.'.format(timer.duration()))
 
 
-# ---- run code ---- #
-
-
-timer = ExecutionTime()
-sample_list = list()
-my_list = [random.randint(1, 888898) for num in
-           range(1, 1000000) if num % 2 == 0]
-print('Finished in {} seconds.'.format(timer.duration()))
