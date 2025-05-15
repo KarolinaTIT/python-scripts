a8c42b053ab3006aed7a695ea574b4209c61464b
KarolinaTIT <karolinaseblikina07@gmail.com>
2025-05-15 11:05:18 +0300

    Update 33_country_code1.py

--- scripts/33_country_code.py
+++ scripts/33_country_code.py
@@ -49,3 +49,67 @@ if __name__ == '__main__':
     json_file_name = sys.argv[2]
     data = get_data(csv_file_name, json_file_name)
     write_data(data)
+
+
+import csv, json
+
+
+with open("33_country_codes.json","r") as file:
+    data=json.load(file)
+    print(data)
+    
+
+
+
+
+
+
+
+codes = {"country": [
+          {
+              "countryCode": "AD",
+              "countryName": "Andorra",
+              "continentName": "Europe"
+          },
+          {
+              "countryCode": "AE",
+              "countryName": "United Arab Emirates",
+              "continentName": "Asia"
+          },
+          {
+              "countryCode": "AF",
+              "countryName": "Afghanistan",
+              "continentName": "Asia"
+          }
+        ]
+    }
+
+codesc=[]
+countries=[]
+continrnt=[]
+
+
+print(codes["country"][0]["countryName"])
+
+for code in data["country"]:
+    codesc.append(code["countryCode"])
+    countries.append(code["countryName"])
+    continrnt.append(code["continentName"])
+
+
+
+print(codesc)
+print(countries)
+print(continrnt)
+
+
+def save_csv(codesc, countries, continrnt):
+    with open("myCSV1.csv", "w", newline="") as file:
+        writer=csv.writer(file)
+        writer.writerow(["country Code","country Name","continent Name"])
+        print(codesc,countries,continrnt)
+        for i in range(1,len(countries)):
+            writer.writerow([codesc[i],countries[i],continrnt[i]])
+            
+        
+save_csv(codesc,countries,continrnt)
